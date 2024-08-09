d9d5e1cfa191cb8c4e9a64eefa272eb5d3462efd
jammy-jar <jrose04.0821@gmail.com>
2024-08-09 16:17:03 +0100

    Style index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,61 +5,24 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <header>
-        <nav className="sticky flex items-center justify-between flex-wrap py-3 px-6 border-b border-slate-900/10">
-          <div className="flex items-center flex-shrink-0 mr-12">
-            <Link href="/">
-              <Image
-                src="/bingecase-banner.svg"
-                width={160}
-                height={48}
-                alt="BingeCase Logo"
-              />
-            </Link>
-          </div>
-          <div className="block sm:hidden">
-            <button className="flex items-center px-3 py-2 border rounded border-slate-900/20 hover:text-slate-900 hover:border-white">
-              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    <div>
+      <section className="container mx-32 mt-40 mb-64 flex w-1/3 items-center">
+        <div>
+          <h2 className="font-bold text-4xl tracking-tight">Compare your favourite Books, Movies, TV Shows and Music!</h2>
+          <p className="mt-10 font-light text-zinc-300 text-lg">Create a Bracket and pick a winner,<br />
+          Or click Search to start make a list or rating!</p>
+          <div className="flex mt-10">
+            <button className="text-lg block border px-5 py-1 rounded-3xl bg-zinc-300 text-zinc-800 border-zinc-800">Bracket Creator</button>
+            <button className="ml-10 text-lg block border px-5 py-1 rounded-3xl text-zinc-300 border-zinc-300">
+              <div className="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                </svg> <p className="ml-3 font-light">Search</p>
+              </div>
             </button>
           </div>
-          <div className="hidden w-full flex-grow sm:flex sm:items-center sm:w-auto">
-            <div className="sm:flex-grow">
-              <Link href="#" className="block mt-4 sm:inline-block sm:mt-0 hover:text-slate-600 mr-12">
-                List
-              </Link>
-              <Link href="#" className="block mt-4 sm:inline-block sm:mt-0 hover:text-slate-600 mr-12">
-                Library
-              </Link>
-              <Link href="./bracket" className="block mt-4 sm:inline-block sm:mt-0 hover:text-slate-600 mr-12">
-                Bracket
-              </Link>
-            </div>
-            <div>
-              <button className="block mt-4 sm:inline-block sm:mt-0">
-                Sign in
-              </button>
-            </div>
-          </div>
-        </nav>
-      </header>
-
-      <main>
-        <section className="flex container mx-auto py-5">
-          <input type="text" placeholder="Search..." className="block sm:inline-block mt-4 sm:mt-0 px-1 py-2 leading-none border rounded text-black"></input>
-
-        </section>
-      </main>
-
-      <footer className="flex justify-between items-center flex-wrap py-2 px-6 border-t border-slate-900/10">
-        <div className="w-full block flex-grow">
-          <div><p className="block">test</p></div>
-          <div>
-            <p className="block">James Rose</p>
-          </div>
         </div>
-      </footer>
-      
+      </section>
     </div>
     );
   }
